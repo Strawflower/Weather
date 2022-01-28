db27e455517ce1393cccd0e02775b731006bdef1
Strawflower <strawflowermine@gmail.com>
2022-01-28 21:32:02 +0300

    38

--- weather.js
+++ weather.js
@@ -1,7 +1,17 @@
 #!/usr/bin/env node
 
 import { getArgs } from './helpers/args.js';
-import { printHelp } from './services/log.services.js';
+import { printHelp, printSucces, printError } from './services/log.services.js';
+import { saveKeyValue } from './services/storage.service.js';
+
+const saveToken = async (token) => {
+    try {
+        await saveKeyValue('token', token);  
+        printSucces('Token saved');
+    } catch (error) {
+        printError(e.message);
+    }
+}
 
 const initCLI = () => {
     const args = getArgs(process.argv);
@@ -9,12 +19,12 @@ const initCLI = () => {
         printHelp();
     }
     if (args.s) {
-        
+       
     }
     if (args.t) {
-        
+        return saveToken(args.t);
     }
 
-};
+}; 
 
 initCLI();
