05f50b6b226e7246518b25c3f09422abdaf6fa5a
Strawflower <strawflowermine@gmail.com>
2022-01-28 04:34:31 +0300

    2

--- weather.js
+++ weather.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js'
 import { printHelp } from './services/log.services.js'
+import { saveKeyValue } from './services/storage.service.js';
 
 const initCLI = () => {
     const args = getArgs(process.argv);
@@ -8,11 +9,11 @@ const initCLI = () => {
     if (args.h) {
         printHelp();
     }
-    if (args.h) {
+    if (args.s) {
         
     }
-    if (args.h) {
-        
+    if (args.t) {
+        saveKeyValue('token', args.t);
     }
     
 };
